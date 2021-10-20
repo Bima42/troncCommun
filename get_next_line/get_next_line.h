658ce13665707c25c68014a2840b806ed7add606
bimasword <tanpau42@gmail.com>
2021-10-20 16:56:11 +0200

    fill .h

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -7,4 +7,9 @@
 # include <unistd.h>
 # include <string.h>
 
+static char     *ft_strjoin(char const *s1, char const *s2);
+static char     *ft_strcpy(char *dest, const char *src);
+static char     *ft_substr(char const *s, unsigned int start, size_t len);
+static char     *ft_strchr(const char *s, int c);
+static size_t   ft_strlen(const char *str);
 #endif
