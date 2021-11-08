f07b9a936f6c45bf2be88127b18d49dccab32efc
bimasword <tanpau42@gmail.com>
2021-11-08 16:59:54 +0100

    mama

--- libft/libft.h
+++ libft/libft.h
@@ -58,7 +58,7 @@ char	*ft_strtrim(char const *s1, char const *set);
 char	*ft_itoa(int n);
 char	**ft_split(char const *s, char c);
 
-//Memory funcitons : 
+//Memory funcitons :
 void	*ft_memset(void *b, int c, size_t len);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
@@ -67,7 +67,7 @@ int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_calloc(size_t count, size_t size);
 void	ft_bzero(void *s, size_t n);
 
-//Chained lists : 
+//Chained lists :
 void	ft_lstadd_front(t_list **alst, t_list *new);
 int		ft_lstsize(t_list *lst);
 void	ft_lstadd_back(t_list **alst, t_list *new);
